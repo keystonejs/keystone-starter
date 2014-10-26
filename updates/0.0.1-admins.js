ed8a9817fdf0aef2968fad1cdf15c2ac16009639
Jed Watson <jed.watson@me.com>
2014-10-26 21:22:40 +1100

    Cleanup

--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -12,38 +12,3 @@ exports.create = {
 		{ 'name.first': 'Admin', 'name.last': 'User', email: 'user@keystonejs.com', password: 'admin', isAdmin: true }
 	]
 };
-
-/*
-
-// This is the long-hand version of the functionality above:
-
-var keystone = require('keystone'),
-	async = require('async'),
-	User = keystone.list('User');
-
-var admins = [
-	{ email: 'user@keystonejs.com', password: 'admin', name: { first: 'Admin', last: 'User' } }
-];
-
-function createAdmin(admin, done) {
-	
-	var newAdmin = new User.model(admin);
-	
-	newAdmin.isAdmin = true;
-	newAdmin.save(function(err) {
-		if (err) {
-			console.error("Error adding admin " + admin.email + " to the database:");
-			console.error(err);
-		} else {
-			console.log("Added admin " + admin.email + " to the database.");
-		}
-		done(err);
-	});
-	
-}
-
-exports = module.exports = function(done) {
-	async.forEach(admins, createAdmin, done);
-};
-
-*/
